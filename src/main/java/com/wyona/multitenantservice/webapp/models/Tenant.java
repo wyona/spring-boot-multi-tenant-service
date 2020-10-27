fbc476f28f5d8b98e8e5b1f220bdd316fc5fd752
Michael Wechner <michael.wechner@wyona.com>
2020-10-27 17:30:08 +0100

    Tenant added

---
+++ src/main/java/com/wyona/multitenantservice/webapp/models/Tenant.java
@@ -0,0 +1,29 @@
+package com.wyona.multitenantservice.webapp.models;
+
+/**
+ *
+ */
+public class Tenant {
+
+    private String name;
+
+    /**
+     *
+     */
+    public Tenant() {
+    }
+
+    /**
+     *
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
