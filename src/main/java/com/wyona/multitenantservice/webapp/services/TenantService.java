1e127b92a7c28a4d113e3eeaaf68c4eca5aca63b
Michael Wechner <michael.wechner@wyona.com>
2021-02-14 00:58:34 +0100

    example UUID added

--- src/main/java/com/wyona/multitenantservice/webapp/services/TenantService.java
+++ src/main/java/com/wyona/multitenantservice/webapp/services/TenantService.java
@@ -7,7 +7,7 @@ public interface TenantService {
 
     /**
      * @param name Tenant name, e.g. "Wyona"
-     * @return UUID of newly created tenant
+     * @return UUID of newly created tenant, e.g. "8ee9f114-9113-404b-8947-2367d4bd0da5"
      */
     String createTenant(String name);
 
